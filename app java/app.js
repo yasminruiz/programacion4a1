385f286a721e5a1ce30b294a4467e925652b2c00
himedeyasser <himedeyasser@gmail.com>
2020-03-04 09:54:00 -0600

    hol

--- app java/app.js
+++ app java/app.js
@@ -1,36 +1,22 @@
-document.addEventListener("DOMContentLoaded",( event) =>{
-    const formAlumnos = document.querySelector("#frmAlumno");
-    formAlumnos.addEventListener("submit", (e)=>{
-        e.preventDefault();
+document.addEventListener("DOMContentLoaded", e=>{
+    const form = document.querySelector("#frmConversores");
+    form.addEventListener("submit", event=>{
+        event.preventDefault();
 
-        const $ = document.querySelector;
-        let codigo = $("#txtCodigoAlumno").value;
-        nombre = document.querySelector("#txtCodigoAlumno").value,
-        Direccion = document.querySelector("#txtDireccionAlumno").value,
-        Telefono = document.querySelector("#txtTelefonoAlumno").value;
-        
-        if('localStorage' in window){
-            window.localStorage.setItem("codigo", codigo);
-            window.localStorage.setItem("nombre", nombre);
-            window.localStorage.setItem("direccion", Direccion);
-            window.localStorage.setItem("telefono", Telefono);
-        }else{
-            alert("almacenamiento local no soportado!!! ")
-        }//cambios
+        let de = document.querySelector("#cboDe").value,
+            a = document.querySelector("#cboA").value,
+            cantidad = document.querySelector("#txtCantidadConversor").value;
+        console.log(de, a, cantidad);
+        let monedas = {
+            "dolar":1,
+            "euro":0.93,
+            "quetzal":7.63,
+            "lempira":24.9,
+            "cordoba":34.19
+        };
+        let $res = document.querySelector("#lblRespuesta");
+        $res.innerHTML = `Respuesta: ${ (monedas[a]/monedas[de]*cantidad).toFixed(2) }`;
     });
-    document.querySelector("#btnRecuperarAlumno").addEventListener("click", (e)=> {
-        if('localstorage' in window){
-            document.querySelector("#txtCodigoAlumno").value = window.localStorage.getItem("codigo");
-            document.querySelector("#txtNombreAlumno").value = window.localStorage.getItem("nombre");
-            document.querySelector("#txtDireccionAlumno").value = window.localStorage.getItem("direccion");
-            document.querySelector("#txtTelefonoAlumno").value = window.localStorage.getItem("telefono");
-        }else{
-            alert("alacenamiento en local no soportado!!");
-        }
-    })
 });
 
 
-/*function init(evrnt){
-    alert("hola la pagina a cargado");
-}*/
