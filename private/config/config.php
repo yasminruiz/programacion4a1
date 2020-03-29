f5b687bb5245b2e0ae6da4a24fa33fd666b8fd80
yasminruiz <rosayasminruiz.12@gmail.com>
2020-03-29 09:24:43 -0600

    config

--- private/config/config.php
+++ private/config/config.php
@@ -1,5 +1,5 @@
 <?php
 
-include('../../Conexion/conexion.php');//conexion xammp
+include('../../Conexion/Db.php');//conexion xammp
 $conexion = new Conexion('localhost','root','','db_app_academica');
 ?>
